e1e70c90d4162bedb206d37998dff02a8df22f75
wangChao <6141364@qq.com>
2025-01-15 11:37:16 +0800

    style: 格式化代码。

--- packages/components/src/Button/src/index.vue
+++ packages/components/src/Button/src/index.vue
@@ -1,25 +1,27 @@
-
 <template>
-  <el-button v-bind="$attrs" @click="handleClick">
+  <el-button
+    v-bind="$attrs"
+    @click="handleClick"
+  >
     <slot />
   </el-button>
 </template>
 
 <script setup lang="ts" name="El2Button">
-const props = defineProps({
-  time: {
-    type: Number,
-    default: 1000,
-  },
-})
-// 抛出事件
-const emits = defineEmits(['click'])
-const record = ref(0)
-const handleClick = () => {
-  let newTime = new Date()
-  if (newTime.getTime() - record.value > props.time) {
-    emits('click')
+  const props = defineProps({
+    time: {
+      type: Number,
+      default: 1000
+    }
+  })
+  // 抛出事件
+  const emits = defineEmits(['click'])
+  const record = ref(0)
+  const handleClick = () => {
+    let newTime = new Date()
+    if (newTime.getTime() - record.value > props.time) {
+      emits('click')
+    }
+    record.value = new Date().getTime()
   }
-  record.value = new Date().getTime()
-}
 </script>
